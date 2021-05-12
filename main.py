22f95eddfa9a863aa78e420214d8ef5cb1037cd0
GregoryBS <aanonimov117@gmail.com>
2021-05-12 17:01:23 +0000

    server implementation with aiohttp

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import asyncio
-import asyncpg
+from aiohttp import web
 
-async def main():
-    conn = await asyncpg.connect('postgresql://anna:yoh@localhost:5432/forums')
+from src import db
+from src.settings import config
+from src.routes import setup_routes
 
-    print("all is ok")
-    await conn.close()
+def main():
+    app = web.Application()
+    setup_routes(app)
+    app['config'] = config
+    app.on_startup.append(db.init_pg)
+    app.on_cleanup.append(db.close_pg)
+    web.run_app(app, port=config['app']['port'])
 
-asyncio.get_event_loop().run_until_complete(main())
+
+main()
